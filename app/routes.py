08baea8c8367b1027a6d51efc195490f40b8b72c
Aria Stroud <stroudaria@gmail.com>
2022-10-23 15:11:22 -0700

    Adds single book endpoint

--- app/routes.py
+++ app/routes.py
@@ -15,6 +15,7 @@ books = [
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+# return all books
 @books_bp.route("", methods=["GET"])
 def handle_books():
     books_response = []
@@ -27,3 +28,15 @@ def handle_books():
             }
         )
     return jsonify(books_response)
+
+# return 1 book by id
+@books_bp.route("/<book_id", methods=["GET"])
+def handle_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
