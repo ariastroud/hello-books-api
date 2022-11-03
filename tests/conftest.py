72ba54a28a278ad26cc32fcf9a5206a9c5f5e30e
Aria Stroud <stroudaria@gmail.com>
2022-11-02 18:44:07 -0700

    Adds test for get one book

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,4 +24,11 @@ def app():
 def client(app):
     return app.test_client()
 
+@pytest.fixture
+def two_saved_books(app):
+    # arrange
+    ocean_book = Book(title="Ocean Book", description="water 4ever")
+    mountain_book = Book(title="Mountain Book", description="Love eat rocks")
 
+    db.session.add_all([ocean_book, mountain_book])
+    db.session.commit()
